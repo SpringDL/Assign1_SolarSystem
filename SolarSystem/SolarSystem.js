138eafce4d09e530df4aaa4bfc0b93a33cf7905c
SpringDL <DanL4@live.ca>
2015-01-12 18:15:45 -0500

    Rest of Planets

--- SolarSystem/SolarSystem.js
+++ SolarSystem/SolarSystem.js
@@ -4,8 +4,9 @@
 
 //Global Variables
 var scene, camera, renderer;
-var sun, mercury, venus, earth;
-var mercuryPivot, venusPivot, earthPivot;
+var sun, mercury, venus, earth, mars, jupiter, saturn, uranus, neptune, pluto;
+var mercuryPivot, venusPivot, earthPivot, marsPivot, jupiterPivot, saturnPivot, uranusPivot, neptunePivot, plutoPivot;
+var saturnRings;
 
 //Initialize
 function init() {
@@ -36,13 +37,25 @@ function createGeometry() {
     //Add Sun to Scene
     scene.add(sun);
 
-    //Create Pivot Points for Planets
+    //Create Pivot Points for Planets   >>  Different Pivots for Different Speeds
     mercuryPivot = new THREE.Object3D();
     sun.add(mercuryPivot);
     venusPivot = new THREE.Object3D();
     sun.add(venusPivot);
     earthPivot = new THREE.Object3D();
     sun.add(earthPivot);
+    marsPivot = new THREE.Object3D();
+    sun.add(marsPivot);
+    jupiterPivot = new THREE.Object3D();
+    sun.add(jupiterPivot);
+    saturnPivot = new THREE.Object3D();
+    sun.add(saturnPivot);
+    uranusPivot = new THREE.Object3D();
+    sun.add(uranusPivot);
+    neptunePivot = new THREE.Object3D();
+    sun.add(neptunePivot);
+    plutoPivot = new THREE.Object3D();
+    sun.add(plutoPivot);
 
     //Create Mercury
     var mercuryGeometry = new THREE.SphereGeometry(5, 20, 20);
@@ -71,18 +84,89 @@ function createGeometry() {
     var earthMaterial = new THREE.MeshLambertMaterial({ color: 0x0099FF });
     earth = new THREE.Mesh(earthGeometry, earthMaterial);
     //Position Earth
-    earth.position.set(0, 0, -140);
+    earth.position.set(0, 0, -135);
     //Add Earth to Scene
     scene.add(earth);
     //Make Child of Pivot Point
     earthPivot.add(earth);
 
+    //Create Mars
+    var marsGeometry = new THREE.SphereGeometry(7, 20, 20);
+    var marsMaterial = new THREE.MeshLambertMaterial({ color: 0xCC6600 });
+    mars = new THREE.Mesh(marsGeometry, marsMaterial);
+    //Position Mars
+    mars.position.set(0, 0, -170);
+    //Add Mars to Scene
+    scene.add(mars);
+    //Make Child of Pivot Point
+    marsPivot.add(mars);
 
+    //Create Jupiter
+    var jupiterGeometry = new THREE.SphereGeometry(23, 20, 20);
+    var jupiterMaterial = new THREE.MeshLambertMaterial({ color: 0xCCCC52 });
+    jupiter = new THREE.Mesh(jupiterGeometry, jupiterMaterial);
+    //Position Jupiter
+    jupiter.position.set(0, 0, -225);
+    //Add Jupiter to Scene
+    scene.add(jupiter);
+    //Make Child of Pivot Point
+    jupiterPivot.add(jupiter);
 
+    //Create Saturn
+    var saturnGeometry = new THREE.SphereGeometry(20, 20, 20);
+    var saturnMaterial = new THREE.MeshLambertMaterial({ color: 0xFF8533 });
+    saturn = new THREE.Mesh(saturnGeometry, saturnMaterial);
+    //Position Saturn
+    saturn.position.set(0, 0, -310);
+    //Add Saturn to Scene
+    scene.add(saturn);
+    //Make Child of Pivot Point
+    saturnPivot.add(saturn);
+
+    //Saturn's Rings
+    var sRingGeometry = new THREE.RingGeometry(23, 35, 32);
+    var sRingMaterial = new THREE.MeshBasicMaterial({ color: 0xFF8533 });
+    saturnRings = new THREE.Mesh(sRingGeometry, sRingMaterial);
+    saturnRings.rotation.set(0, 30.15, 0);
+    scene.add(saturnRings);
+    saturn.add(saturnRings);
+
+    //Create Uranus
+    var uranusGeometry = new THREE.SphereGeometry(18, 20, 20);
+    var uranusMaterial = new THREE.MeshLambertMaterial({ color: 0x94FFB8 });
+    uranus = new THREE.Mesh(uranusGeometry, uranusMaterial);
+    //Position Uranus
+    uranus.position.set(0, 0, -400);
+    //Add Uranus to Scene
+    scene.add(uranus);
+    //Make Child of Pivot Point
+    uranusPivot.add(uranus);
+
+    //Create Neptune
+    var neptuneGeometry = new THREE.SphereGeometry(19, 20, 20);
+    var neptuneMaterial = new THREE.MeshLambertMaterial({ color: 0x0052CC });
+    neptune = new THREE.Mesh(neptuneGeometry, neptuneMaterial);
+    //Position Neptune
+    neptune.position.set(0, 0, -475);
+    //Add Neptune to Scene
+    scene.add(neptune);
+    //Make Child of Pivot Point
+    neptunePivot.add(neptune);
+
+    //Create Pluto
+    var plutoGeometry = new THREE.SphereGeometry(5, 20, 20);
+    var plutoMaterial = new THREE.MeshLambertMaterial({ color: 0xE0E0EB });
+    pluto = new THREE.Mesh(plutoGeometry, plutoMaterial);
+    //Position Pluto
+    pluto.position.set(0, 0, -535);
+    //Add Pluto to Scene
+    scene.add(pluto);
+    //Make Child of Pivot Point
+    plutoPivot.add(pluto);
 
     //Position Camera
     camera.position.x = 0;
-    camera.position.y = 250;
+    camera.position.y = 450;
     camera.position.z = 0;
     camera.lookAt(scene.position);
 
@@ -92,15 +176,24 @@ function createGeometry() {
 
     //Add Spotlight for lighting
     var spotLight = new THREE.SpotLight(0xffffff);
-    spotLight.position.set(0, 1000, 0);
+    spotLight.position.set(0, 500, 0);
     scene.add(spotLight);
+    var spotLight2 = new THREE.SpotLight(0xffffff);
+    spotLight2.position.set(0, 500, 1000);
+    scene.add(spotLight2);
 }
 
 //
 function animate() {
-    //mercuryPivot.rotation.y -= 0.01;
-    //venusPivot.rotation.y -= 0.01;
-    //earthPivot.rotation.y -= 0.01;
+    mercuryPivot.rotation.y -= 0.09;
+    venusPivot.rotation.y -= 0.08;
+    earthPivot.rotation.y -= 0.07;
+    marsPivot.rotation.y -= 0.06;
+    jupiterPivot.rotation.y -= 0.05;
+    saturnPivot.rotation.y -= 0.04;
+    uranusPivot.rotation.y -= 0.03;
+    neptunePivot.rotation.y -= 0.02;
+    plutoPivot.rotation.y -= 0.01;
 
     // render using requestAnimationFrame
     requestAnimationFrame(animate);
